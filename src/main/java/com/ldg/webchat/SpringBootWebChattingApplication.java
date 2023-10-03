720ad577b93d72414c01ac22683d295fcc10c456
ldg030201 <ldg030201>
2023-10-03 18:49:45 +0900

    init

---
+++ src/main/java/com/ldg/webchat/SpringBootWebChattingApplication.java
@@ -0,0 +1,13 @@
+package com.ldg.webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldg/webchat/SpringBootWebChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldg.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
